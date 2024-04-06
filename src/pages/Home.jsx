8e649d2711fb5059d05a389b982a9831280f8d28
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-04-06 21:53:33 +0530

    update trending_product  section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import React from "react";
-
+import React ,{useState,useEffect} from "react";
+import '../style/home.css';
 
 import Helmet from "../components/Helmet/Helmet";
-import '../style/home.css';
 import Services from "../services/Services";
+import ProductList from "../components/Ui/ProductList";
+
+import product from "../assets/data/products"
 
 import { Container, Row , Col } from "reactstrap";
 import heroImg from '../assets/images/hero-img.png'
@@ -11,7 +13,16 @@ import {Link} from "react-router-dom";
 import { motion} from "framer-motion";
 
 const Home = ()  => {
+
+  const [data,setData] = useState(product)
   const year = new Date().getFullYear()
+
+  useEffect(()=>{
+    const filterdProduct = product.filter(
+        (item)=> item.category === 'chair');
+    setData(filterdProduct);
+  },[]);
+
   return <Helmet title={'Home'}>
     <section className="hero__section">
       <Container>
@@ -35,6 +46,16 @@ const Home = ()  => {
       </Container>
     </section>
     <Services/>
+    <section className="trending__products">
+      <Container>
+        <Row>
+          <Col lg='12' className="text-center">
+            <h2 className="section__title">Trending Products</h2>
+          </Col>
+          <ProductList data={data}/>
+        </Row>
+      </Container>
+    </section>
   </Helmet>
 };
 
