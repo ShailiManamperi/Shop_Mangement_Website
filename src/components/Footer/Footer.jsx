5680c75e6cb6f38f8fe194402143bcc7c843da27
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-04-08 12:08:50 +0530

    update footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,99 @@
 import React from "react";
+import './footer.css';
+
+import { Container, Row , Col ,ListGroup , ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/eco-logo.png";
 
 const Footer = () =>{
-    return <div>Footer</div>;
+    const year = new Date().getFullYear();
+    return <footer className="footer">
+        <Container>
+            <Row>
+                <Col lg='4'>
+                    <div className="logo">
+                        <div>
+                            <h1 className="text-white">Wimalasiri Furniture</h1>
+                        </div>
+                    </div>
+                    <p className="footer__text mt-4">
+                        Discover quality furnishings at our inviting shop. From modern sofas to classic tables, find the perfect pieces for your home.
+                    </p>
+                </Col>
+                <Col lg='3'>
+                    <div className="footer__quick-links">
+                        <h4 className="quick__links-titles">Top Category</h4>
+                        <ListGroup className="mb-3">
+                            <ListGroupItem
+                                className='ps-0 border-0'>
+                                <Link to="#">Mobiles Phones</Link>
+                            </ListGroupItem>
+                            <ListGroupItem
+                                className='ps-0 border-0 '>
+                                <Link to="#">Modern Sofa</Link>
+                            </ListGroupItem>
+                            <ListGroupItem
+                                className='ps-0 border-0 '>
+                                <Link to="#">Arm Chair</Link>
+                            </ListGroupItem>
+                            <ListGroupItem
+                                className='ps-0 border-0 '>
+                                <Link to="#">Smart Watches</Link>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </div>
+                </Col>
+                <Col lg='2'>
+                    <div className="footer__quick-links">
+                        <h4 className="quick__links-titles">Useful Links</h4>
+                        <ListGroup>
+                            <ListGroupItem className='ps-0 border-0'>
+                                <Link to="/shop">Shop</Link>
+                            </ListGroupItem>
+                            <ListGroupItem className='ps-0 border-0'>
+                                <Link to="/cart">Cart</Link>
+                            </ListGroupItem>
+                            <ListGroupItem className='ps-0 border-0'>
+                                <Link to="/login">Login</Link>
+                            </ListGroupItem>
+                            <ListGroupItem className='ps-0 border-0'>
+                                <Link to="#">Privacy Policy</Link>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </div>
+
+                </Col>
+                <Col lg='3'>
+                    <div className="footer__quick-links">
+                        <h4 className="quick__links-titles">Contact</h4>
+                        <ListGroup className="footer__contact">
+                            <ListGroupItem
+                                className='ps-0 border-0 d-flex align-items-center gap-2'>
+                                <span><i className="ri-map-pin-line"></i></span>
+                                <p>25/6 , Dias Place , Panadura</p>
+                            </ListGroupItem>
+                            <ListGroupItem
+                                className='ps-0 border-0 d-flex align-items-center gap-2'>
+                                <span><i className="ri-phone-line"></i></span>
+                                <p>+94-714064878</p>
+                            </ListGroupItem>
+                            <ListGroupItem
+                                className='ps-0 border-0 d-flex align-items-center gap-2'>
+                                <span><i className="ri-mail-line"></i></span>
+                                <p>shailimanamperi2002@gmail.com</p>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </div>
+
+                </Col>
+                <Col lg='12'>
+                    <p className="footer__copyright">
+                        Copyright {year} developed by Shaili Manamperi. All rights reserved.
+                    </p>
+                </Col>
+            </Row>
+        </Container>
+    </footer>
 }
 
 export default Footer;
