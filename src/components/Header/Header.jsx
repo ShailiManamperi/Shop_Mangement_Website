e1cd2f8e2b778adf77ac0c7e3035cffdf5e690ca
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-04-26 16:45:00 +0530

    fixed cart count

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,8 @@ import {motion} from "framer-motion";
 import logo from '../../assets/images/eco-logo.png'
 import userIcon from '../../assets/images/user-icon.png'
 
-import {Container , Row} from "reactstrap";
+import {Container , Row } from "reactstrap";
+import { useSelector } from "react-redux";
 
 const nav__link = [
     {
@@ -28,6 +29,8 @@ const nav__link = [
 const Header = () =>{
 
     const headerRef = useRef(null)
+
+    const totalQuantity = useSelector(state => state.cart.totalQuantity)
 
     const menuRef = useRef(null);
     const stickyHeaderFunc = () =>{
@@ -77,7 +80,7 @@ const Header = () =>{
                             </span>
                             <span className="cart_icon">
                                 <i className="ri-shopping-bag-line"></i>
-                                <span className="badge">1</span>
+                                <span className="badge">{totalQuantity}</span>
                             </span>
                             <span>
                                 <motion.img whileTap={{scale: 1.2}} src={userIcon} alt="usericon"/>
