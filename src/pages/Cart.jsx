7b4f780f1fab7385f74173f4c5237d1b7e5dd93f
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-06-07 22:18:42 +0530

    update cart (with subtotal)

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,10 +7,12 @@ import { Container,Row,Col } from "reactstrap";
 import {motion} from "framer-motion";
 import {cartActions} from "../redux/slices/cartSlice";
 import {useSelector , useDispatch} from "react-redux";
+import {Link} from "react-router-dom";
 
 const Cart = () =>  {
 
     const cartItems = useSelector(state => state.cart.cartItems)
+    const totalAmount = useSelector(state => state.cart.totalAmount)
 
     return (
         <Helmet title='Cart'>
@@ -45,7 +47,20 @@ const Cart = () =>  {
                             }
 
                         </Col>
-                        <Col lg={3}></Col>
+                        <Col lg={3}>
+                            <div>
+                                <h6 className='d-flex align-items-center justify-content-between'>
+                                    Subtotal
+                                    <span className={'fs-4 fw-bold'}>${totalAmount}</span>
+                                </h6>
+
+                            </div>
+                            <p className={'fs-6 mt-2'}>taxes and shipping will calculate in checkout.</p>
+                            <div>
+                                <button className='buy_btn w-100'><Link to={'/checkout'}>Checkout</Link></button>
+                                <button className='buy_btn w-100 mt-3'><Link to={'/shop'}>Countinue Shopping</Link></button>
+                            </div>
+                        </Col>
                     </Row>
                 </Container>
             </section>
