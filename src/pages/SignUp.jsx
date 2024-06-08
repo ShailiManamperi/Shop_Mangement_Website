756b7ae2a038005251e6a74030f5b70cd73ff9fb
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-06-08 22:43:12 +0530

    update login file

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,50 @@
-import React from "react";
+import React,{useState} from "react";
+import Helmet from "../components/Helmet/Helmet";
+import { Container, Row, Col, Form,FormGroup} from "reactstrap";
+import { Link } from "react-router-dom";
+import '../style/login.css';
 
-const SignUp = () => {
-    return <div>Signup</div>;
+const Signup = () => {
+    const [username,setUsername]= useState('')
+    const [email,setEmail]= useState('')
+    const [password,setPassword]= useState('')
+    const [file,setFile]= useState(null)
+
+    return (
+        <Helmet title={'Signup'}>
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg={6} className={'m-auto text-center'}>
+                            <h3 className={'fw-bold mb-4'}>Signup</h3>
+
+                            <Form className={'auth__form'}>
+                                <FormGroup className={'form__group'}>
+                                    <input type={'text'} placeholder={'Enter Username'}
+                                           value={username} onChange={e=> setUsername(e.target.value)}/>
+                                </FormGroup>
+                                <FormGroup className={'form__group'}>
+                                    <input type={'email'} placeholder={'Enter your email'}
+                                           value={email} onChange={e=> setEmail(e.target.value)}/>
+                                </FormGroup>
+                                <FormGroup className={'form__group'}>
+                                    <input type={'password'} placeholder={'Enter your password'}
+                                           value={password} onChange={e=> setPassword(e.target.value)}/>
+                                </FormGroup>
+                                <FormGroup className={'form__group'}>
+                                    <input type={'file'} onChange={e=> setFile(e.target.value)}/>
+                                </FormGroup>
+
+                                <button type={'submit'} className='buy_btn auth__btn '>Create an account</button>
+                                <p>Already have an account? <Link to={'/login'}>Login</Link></p>
+
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        </Helmet>
+    );
 };
 
-export default SignUp;
-
+export default  Signup;
