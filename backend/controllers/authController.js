4d6d5549a68a69191ed55f2e9b6f259f3d9c8801
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-06-14 15:44:35 +0530

    update auth controller with login function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,9 @@
 import User from "../models/User.js";
+
 import bcrypt from "bcryptjs";
 import jwt from 'jsonwebtoken';
 import Users from "../routes/users.js";
+//const User = require("../models/User.js");
 
 
 //user registration
@@ -34,9 +36,11 @@ export const register = async (req,res) =>{
 //user login
 export const login = async (req,res) =>{
     const email = req.body.email
+    //console.log(email)
 
     try{
         const user = await User.findOne({email})
+        //console.log(user)
 
         //if user doesn't exist
         if(!user){
@@ -61,15 +65,30 @@ export const login = async (req,res) =>{
             {expiresIn: '15d'}
         );
 
-        //set token in the browser cookies and send the responsive to the client
-        res.cookies('accessToken',token,{
+        //console.log(token)
 
-        })
+        //set token in the browser cookies and send the responsive to the client
+        // res.cookies('accessToken',token,{
+        //     httpOnly: true,
+        //     expires:token.expiresIn
+        // }).status(200).json({
+        //     success:true,
+        //     message:'sucefully login',
+        //     data:{ ...rest}
+        // })
+        res.cookie('accessToken', token, {
+            httpOnly: true,
+            expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000) // 15 days
+        }).status(200).json({
+            success: true,
+            message: 'Successfully logged in',
+            data: { ...rest }
+        });
+
 
     }catch (err){
-
+        console.log(err)
+        return res.status(500).json({success:false,message:"Failed to login."})
     }
-
-
 
 };
