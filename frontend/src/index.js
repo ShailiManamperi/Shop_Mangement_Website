eee5b4b27d597fcb76c42cd8f364dd80a9b7e284
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-06-23 22:18:20 +0530

    drop some files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,20 +10,24 @@ import { Provider } from "react-redux";
 import { ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
+import {AuthContextProvider} from "./context/AuthContext";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
-          <Provider store={store}>
-              <ToastContainer
-                  theme="dark"
-                  position="top-right"
-                  autoClose={3000}
-                  closeOnClick
-                  pauseOnHover={false}
-              />
-              <App />
-          </Provider>
-      </BrowserRouter>
+      <AuthContextProvider>
+          <BrowserRouter>
+              <Provider store={store}>
+                  <ToastContainer
+                      theme="dark"
+                      position="top-right"
+                      autoClose={3000}
+                      closeOnClick
+                      pauseOnHover={false}
+                  />
+                  <App />
+              </Provider>
+          </BrowserRouter>
+      </AuthContextProvider>
   </React.StrictMode>
 );
