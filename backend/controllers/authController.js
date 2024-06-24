b31473559e7b6a90a607cd7f7aa1bc64b1fea750
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-06-24 15:21:35 +0530

    update auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,37 +1,33 @@
 import express from "express";
 import User from "../models/User.js";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import jwt from 'jsonwebtoken';
 
 //user registration
-export const register = async (req: express.Request, res: express.Response) =>{
-    try{
-
-        // hashing password
-        const salt = bcrypt.genSaltSync(10)
-        const hash = bcrypt.hashSync(req.body.password,salt)
+export const register = async (req, res) => {
+    try {
+        // Hashing password
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
 
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
             password: hash,
             role: req.body.role
-        })
+        });
 
         await newUser.save();
 
-        res.status(200).json({success:true,message:"Sucessfully saved...."})
+        res.status(200).json({ success: true, message: "Successfully saved...." });
 
-    }catch (err){
-
-        res.status(500).json({success:false,message:"Failed to created...."})
-
+    } catch (err) {
+        res.status(500).json({ success: false, message: "Failed to create...." });
     }
-
 };
 
 //user login
-export const login = async (req: express.Request, res: express.Response) =>{
+export const login = async (req, res) =>{
     const email = req.body.email
 
     try{
