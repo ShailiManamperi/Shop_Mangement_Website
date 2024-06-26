534612a1abff60f56264bb3d7e848967dbea1e4f
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-06-26 16:32:58 +0530

    update admin nav bar

--- admin/src/pages/AdminNav.jsx
+++ admin/src/pages/AdminNav.jsx
@@ -1,7 +1,80 @@
 import React from "react";
+import { Container,Row,Col} from "reactstrap";
+import { motion } from "framer-motion";
+import logo from '../assets/images/eco-logo.png';
+import userIcon from '../assets/images/user-icon.png';
+import '../style/adminNav.css';
+import { NavLink } from "react-router-dom";
+
+
+const admin_nav = [
+    {
+        display:'Dashboard',
+        path: '/dashboard'
+    },
+    {
+        display:'All-Products',
+        path: '/dashboard/all-products'
+    },
+    {
+        display:'Orders',
+        path: '/dashboard/orders'
+    },
+    {
+        display:'Users',
+        path: '/dashboard/users'
+    },
+]
+
 
 const AdminNav = () =>{
-    return <div>Admin Nav</div>
+    return (
+        <>
+            <header className='admin__header'>
+                <div className='admin_nav_top'>
+                    <Container>
+                        <div className='admin-nav-wrapper-top'>
+                            <div className='logo'>
+                                    <h2>Wimalasiri Furniture</h2>
+                            </div>
+
+                            <div className='search__box'>
+                                <input type='text' placeholder='Search...'/>
+                                <span><i className='ri-search-line'></i></span>
+                            </div>
+                            <div className="admin__nav-top-right">
+                                <span><i className='ri-notification-3-line'></i></span>
+                                <span><i className='ri-settings-2-line'></i></span>
+                                <motion.img
+                                    whileTap={{ scale: 1.2 }}
+                                    src={userIcon}
+                                    alt="usericon"
+                                />
+                            </div>
+                        </div>
+                    </Container>
+                </div>
+            </header>
+            <section className="admin__menu">
+                <Container>
+                    <Row>
+                        <div className="admin__navigation">
+                            <ul className='admin__menu-list'>
+                                {
+                                    admin_nav.map((item ,index)=>(
+                                        <li className="admin__menu-item" key={index}>
+                                            <NavLink to={item.path} className={navClass=>navClass.isActive ? 'active__admin_menu' : ''}>
+                                                {item.display}</NavLink>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    </Row>
+                </Container>
+            </section>
+        </>
+    )
 };
 
 export default AdminNav;
