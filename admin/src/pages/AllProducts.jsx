e08e3e6051a1d32d75f0fb94710c4d05dc085edc
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-06-27 21:16:06 +0530

    add all-product files

--- admin/src/pages/AllProducts.jsx
+++ admin/src/pages/AllProducts.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import { Container,Row,Col} from "reactstrap";
+import Productimg from '../assets/images/double-sofa-03.png';
+import '../style/allProducts.css';
 
 
 const AllProducts = () =>{
-    return (
-        <div>All products</div>
-    )
+    return <section>
+        <Container>
+            <Row>
+                <Col lg='12'>
+                    <table className={'table table-bordered'}>
+                        <thead>
+                            <tr>
+                                <th>Image</th>
+                                <th>Title</th>
+                                <th>Category</th>
+                                <th>Price</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><img src={Productimg} alt={'Sofa'}/></td>
+                                <td>Amazon Brand Modern Sofa</td>
+                                <td>Sofa</td>
+                                <td>Rs.220000.00</td>
+                                <td><button className='btn btn-danger'>Delete</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </Col>
+            </Row>
+        </Container>
+    </section>
+
 };
 
 export default AllProducts;
