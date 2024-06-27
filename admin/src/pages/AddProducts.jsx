8d65e70169c0b568701e03f28827a906492c4fab
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-06-27 16:02:46 +0530

    update add-product file

--- admin/src/pages/AddProducts.jsx
+++ admin/src/pages/AddProducts.jsx
@@ -4,6 +4,7 @@ import { Container, Row, Col, Form, FormGroup } from "reactstrap";
 import '../style/addProduct.css';
 import { toast } from "react-toastify";
 import axios from "axios";
+import {Navigate, useNavigate} from "react-router-dom";
 
 const AddProduct = () => {
     const [enterTitle, setEnterTitle] = useState('');
@@ -14,9 +15,11 @@ const AddProduct = () => {
     const [enterImage, setImage] = useState(null);
     const [loading, setLoading] = useState(false);
 
+    const navigate = useNavigate();
+
     const addProduct = async (e) => {
         e.preventDefault();
-
+        setLoading(true);
         const productData = {
             productName: enterTitle,
             category: enterCategory,
@@ -24,9 +27,7 @@ const AddProduct = () => {
             shortDesc: enterShortDesc,
             description: enterDescription,
         };
-        console.log(productData);
 
-        setLoading(true);
         try {
             const response = await axios.post("http://localhost:4000/item/", productData, {
                 headers: {
@@ -43,9 +44,10 @@ const AddProduct = () => {
                     }
                 });
             }
-
             setLoading(false);
             toast.success('Product saved successfully...');
+            navigate('/dashboard/all-products');
+
         } catch (err) {
             setLoading(false);
             toast.error('Something went wrong...');
@@ -59,61 +61,65 @@ const AddProduct = () => {
                 <Container>
                     <Row>
                         <Col lg='12'>
-                            <h4 className='mb-5'>Add Products</h4>
-                            <Form onSubmit={addProduct}>
-                                <FormGroup className="form__group">
-                                    <span>Product Title</span>
-                                    <input type='text'
-                                           placeholder='Double Sofa'
-                                           value={enterTitle}
-                                           onChange={e => setEnterTitle(e.target.value)}
-                                           required />
-                                </FormGroup>
-                                <FormGroup className="form__group">
-                                    <span>Short Description</span>
-                                    <input type='text'
-                                           placeholder='Short description...'
-                                           value={enterShortDesc}
-                                           onChange={e => setShortDesc(e.target.value)}
-                                           required />
-                                </FormGroup>
-                                <FormGroup className="form__group">
-                                    <span>Description</span>
-                                    <input type='text'
-                                           placeholder='Description...'
-                                           value={enterDescription}
-                                           onChange={e => setDescription(e.target.value)}
-                                           required />
-                                </FormGroup>
-                                <div className='d-flex align-items-center justify-content-between gap-5'>
-                                    <FormGroup className="form__group w-50">
-                                        <span>Price</span>
-                                        <input type='number'
-                                               placeholder='Price'
-                                               value={enterPrice}
-                                               onChange={e => setPrice(e.target.value)}
-                                               required />
-                                    </FormGroup>
-                                    <FormGroup className="form__group w-50">
-                                        <span>Category</span>
-                                        <select className='w-100 p-2'
-                                                value={enterCategory}
-                                                onChange={e => setCategory(e.target.value)}>
-                                            <option value='chair'>Chair</option>
-                                            <option value='sofa'>Sofa</option>
-                                            <option value='mobile'>Mobile</option>
-                                            <option value='watch'>Watch</option>
-                                        </select>
-                                    </FormGroup>
-                                </div>
-                                <FormGroup className="form__group">
-                                    <span>Product Image</span>
-                                    <input type='file'
-                                           onChange={e => setImage(e.target.files[0])}
-                                           required />
-                                </FormGroup>
-                                <button className="primary__btn btn" type='submit'>Add Product</button>
-                            </Form>
+                            {
+                                loading ? <h4 className='py-5'>Loading....</h4> : <>
+                                    <h4 className='mb-5'>Add Products</h4>
+                                    <Form onSubmit={addProduct}>
+                                        <FormGroup className="form__group">
+                                            <span>Product Title</span>
+                                            <input type='text'
+                                                   placeholder='Double Sofa'
+                                                   value={enterTitle}
+                                                   onChange={e => setEnterTitle(e.target.value)}
+                                                   required />
+                                        </FormGroup>
+                                        <FormGroup className="form__group">
+                                            <span>Short Description</span>
+                                            <input type='text'
+                                                   placeholder='Short description...'
+                                                   value={enterShortDesc}
+                                                   onChange={e => setShortDesc(e.target.value)}
+                                                   required />
+                                        </FormGroup>
+                                        <FormGroup className="form__group">
+                                            <span>Description</span>
+                                            <input type='text'
+                                                   placeholder='Description...'
+                                                   value={enterDescription}
+                                                   onChange={e => setDescription(e.target.value)}
+                                                   required />
+                                        </FormGroup>
+                                        <div className='d-flex align-items-center justify-content-between gap-5'>
+                                            <FormGroup className="form__group w-50">
+                                                <span>Price</span>
+                                                <input type='number'
+                                                       placeholder='Price'
+                                                       value={enterPrice}
+                                                       onChange={e => setPrice(e.target.value)}
+                                                       required />
+                                            </FormGroup>
+                                            <FormGroup className="form__group w-50">
+                                                <span>Category</span>
+                                                <select className='w-100 p-2'
+                                                        value={enterCategory}
+                                                        onChange={e => setCategory(e.target.value)}>
+                                                    <option value='chair'>Chair</option>
+                                                    <option value='sofa'>Sofa</option>
+                                                    <option value='mobile'>Mobile</option>
+                                                    <option value='watch'>Watch</option>
+                                                </select>
+                                            </FormGroup>
+                                        </div>
+                                        <FormGroup className="form__group">
+                                            <span>Product Image</span>
+                                            <input type='file'
+                                                   onChange={e => setImage(e.target.files[0])}
+                                                   required />
+                                        </FormGroup>
+                                        <button className="primary__btn btn" type='submit'>Add Product</button>
+                                    </Form>
+                                </>
+                            }
                         </Col>
                     </Row>
                 </Container>
